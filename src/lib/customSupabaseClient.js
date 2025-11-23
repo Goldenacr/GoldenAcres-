b76335d2c2bfedc76562ded2b4996421fd426f65
Goldenacr <codmnovelah115+random20@gmail.com>
2025-11-23 19:40:39 +0000

    Add files via upload

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://vcbuiawcyukotjomcmiz.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjYnVpYXdjeXVrb3Rqb21jbWl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNTczMzcsImV4cCI6MjA3NzgzMzMzN30.rWkk0gHMu9bvs8TXmKlgXuriuS3DNHPtl93g0eIms18';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+export default customSupabaseClient;
+
+export { 
+    customSupabaseClient,
+    customSupabaseClient as supabase,
+};
